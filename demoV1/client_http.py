ba64d42c83f03fe10a8f20e4fa03fe4c577b073b
Zihao <74972324+noil-lion@users.noreply.github.com>
2022-08-17 14:44:15 +0800

    Add files via upload

---
+++ demoV1/client_http.py
@@ -0,0 +1,104 @@
+import gevent.ssl
+import numpy as np
+import tritonclient.http as httpclient
+
+
+def client_init(url="localhost:40183",
+                ssl=False, key_file=None, cert_file=None, ca_certs=None, insecure=False,
+                verbose=False):
+    """
+    :param url:
+    :param ssl: Enable encrypted link to the server using HTTPS
+    :param key_file: File holding client private key
+    :param cert_file: File holding client certificate
+    :param ca_certs: File holding ca certificate
+    :param insecure: Use no peer verification in SSL communications. Use with caution
+    :param verbose: Enable verbose output
+    :return:
+    """
+    if ssl:
+        ssl_options = {}
+        if key_file is not None:
+            ssl_options['keyfile'] = key_file
+        if cert_file is not None:
+            ssl_options['certfile'] = cert_file
+        if ca_certs is not None:
+            ssl_options['ca_certs'] = ca_certs
+        ssl_context_factory = None
+        if insecure:
+            ssl_context_factory = gevent.ssl._create_unverified_context
+        triton_client = httpclient.InferenceServerClient(
+            url=url,
+            verbose=verbose,
+            ssl=True,
+            ssl_options=ssl_options,
+            insecure=insecure,
+            ssl_context_factory=ssl_context_factory)
+    else:
+        triton_client = httpclient.InferenceServerClient(
+            url=url, verbose=verbose)
+
+    return triton_client
+
+
+def infer(triton_client, model_name,
+          input0='INPUT__0',output0='OUTPUT__0', 
+          request_compression_algorithm=None,
+          response_compression_algorithm=None):
+    """
+    :param triton_client:
+    :param model_name:
+    :param input0:
+    :param input1:
+    :param output0:
+    :param output1:
+    :param request_compression_algorithm: Optional HTTP compression algorithm to use for the request body on client side.
+            Currently supports "deflate", "gzip" and None. By default, no compression is used.
+    :param response_compression_algorithm:
+    :return:
+    """
+    inputs = []
+    outputs = []
+    # batch_size=8
+    # 如果batch_size超过配置文件的max_batch_size，infer则会报错
+    # INPUT0、INPUT1为配置文件中的输入节点名称
+    inputs.append(httpclient.InferInput(input0, [1, 1, 128, 160, 112], "FP32"))
+
+    # Initialize the data
+    # np.random.seed(2022)
+    inputs[0].set_data_from_numpy(np.random.random([1, 1, 128, 160, 112]).astype(np.float32), binary_data=False)
+
+    # OUTPUT0、OUTPUT1为配置文件中的输出节点名称
+    outputs.append(httpclient.InferRequestedOutput(output0, binary_data=False))
+    query_params = {'test_1': 1, 'test_2': 2}
+    results = triton_client.infer(
+        model_name=model_name,
+        inputs=inputs,
+        outputs=outputs,
+        request_compression_algorithm=request_compression_algorithm,
+        response_compression_algorithm=response_compression_algorithm)
+
+
+if __name__ == '__main__':
+    import time
+
+    client = client_init()
+
+    s = time.time()
+
+    infer(triton_client=client, model_name='airway')
+    #
+    # infer(triton_client=client, model_name='tf_graphdef')
+    #
+    # infer(triton_client=client, model_name='torch_model',
+    #       input0='INPUT__0', input1='INPUT__1',
+    #       output0='OUTPUT__0', output1='OUTPUT__1')
+    #
+    # infer(triton_client=client, model_name='tf_onnx',
+    #       input0='INPUT:0', input1='INPUT:1')
+
+    # infer(triton_client=client, model_name='torch_onnx')
+
+    # infer(triton_client=client, model_name='trt_model')
+
+    print("http infer: {}".format(time.time() - s))
---
+++ demoV1/readme.md
@@ -0,0 +1,59 @@
+# demo-V1
+基于docker构建torch模型部署环境triton inference server。
+实例基于已经构建模型仓库并写好基本的配置文件，实例模型为TorchScript模型model.pt
+
+## 环境
+CUDA：11.6;  
+Docker: 20.10.17;  
+OS：20.4.1-Ubuntu;  
+GPU：NVIDIA A100-PCIE-40GB;  
+CPU：Intel(R) Xeon(R) Gold 6330 CPU @ 2.00GHz;  
+Repository: nvcr.io/nvidia/tritonserver-22.04-py3; 
+
+## 指令
+* triton服务启动，配置端口映射，并指定模型仓库路径:  
+```
+cd /full/path/triton/  
+```
+CPU版:  
+```
+docker run --rm -p8000:8000 -p8001:8001 -p8002:8002 -v /full/path/triton/model_repository:/models nvcr.io/nvidia/tritonserver:22.04-py3 tritonserver --model-repository=/models --strict-model-config=false  
+```
+GPU版：  
+```
+docker run --gpus '"device=1"' --rm -p8000:8000 -p8001:8001 -p8002:8002 -v /full/path/triton/model_repository:/models nvcr.io/nvidia/tritonserver:22.04-py3 tritonserver --model-repository=/models --strict-model-config=false
+```
+* 启动服务后，测试是否可连接
+```
+curl -v localhost:40184/v2/health/ready
+
+//以下输出连接成功
+> GET /v2/health/ready HTTP/1.1
+> Host: localhost:40184
+> User-Agent: curl/7.68.0
+> Accept: */*
+> 
+* Received HTTP/0.9 when not allowed
+
+* Closing connection 0
+curl: (1) Received HTTP/0.9 when not allowed
+```
+
+* 启动客户端，发起推理请求  
+```
+grpc版：  
+python client_grpc.py  
+http版：  
+python client_http.py  
+```
+* 测试结果  
+Input: np.random.random([1, 1, 128, 160, 112]).astype(np.float32)  
+CPU版   
+grpc infer: 19.465958833694458, 12.187530040740967,  17.18602156639099  
+http infer: 7.644216299057007, 12.498617887496948, 11.113388538360596   
+GPU版   
+grpc infer: 13.133647918701172, 14.847572088241577, 12.151828050613403  
+http infer: 6.692427396774292, 6.625612735748291, 6.751987934112549   
+* 结果分析  
+  基于tritonclient构建的简版triton client端，生成模型的输入数据，对比了基于不同环境部署下的单个模型实例推理服务延迟时间，同时对比了基于不同数据传输协议下的服务延迟时间。总体而言在GPU环境下部署的服务延时会更低，传输协议的比对结果是http对于当前实例数据的请求响应更快，但这不是绝对的。
+  
